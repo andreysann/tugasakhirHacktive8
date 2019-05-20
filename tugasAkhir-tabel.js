870c9a538fead22793a886ef3b288f42897fdce7
anna <annaandreys14@gmail.com>
2019-05-20 21:02:32 +0700

    1st update

--- tugasAkhir-tabel.js
+++ tugasAkhir-tabel.js
@@ -1,3 +1,26 @@
+var nomor = 6;
+
+function tambah(){
+  var form = document.getElementById('form');
+  var newrow = form.insertRow(nomor);
+
+  var kol1 = newrow.insertCell(0);
+  var kol2 = newrow.insertCell(1);
+  var kol3 = newrow.insertCell(2);
+  var kol4 = newrow.insertCell(3);
+  var kol5 = newrow.insertCell(4);
+
+  kol1.innerHTML= nomor;
+  kol2.innerHTML= '<input class="jenisBarang" type="text">';
+  kol3.innerHTML= '<center>Rp.<input class="harga" type="text" size=7></center>';
+  kol4.innerHTML= '<center><input class="qty" type="text" size=5></center>';
+  kol5.innerHTML= '<center>Rp.<input class="jumlah" type="text" size=10 readonly></center>';
+
+  nomor = nomor + 1;
+}
+
+
+
 function hitung(){
 var txtHarga1 = document.getElementById('harga1');
 var txtQty1 = document.getElementById('qty1');
